02e4bc117879754161ba4e48b8f7508d30773466
Alisher06092003 <qurbonova858@gmail.com>
2025-05-01 12:47:26 -0700

    Bu yerda dir name qoshoshilgan

--- index.mjs
+++ index.mjs
@@ -1,16 +1,33 @@
-import express from 'express'  
-import bp from 'body-parser'   
+import express from 'express';  
+import bp from 'body-parser';   
+import path from 'path';
 
 
 const app = express();
 const port = 1212;
-
-
 const data = [];
 
-app.use(bp.json());     
+const __dirname = path.resolve();
 
+app.use(bp.json());
 
+// All requset har qanday requsetlarni qabul qiladigon yani Post dan tortib Deletgacha
+// app.all('/all' , (req,res, next) => {
+//    console.log('All dan keyingilari ishlasin');
+//    next();
+// });
+
+app.get('/users/alisher', (req,res) => {
+    res.sendFile(__dirname + '/alisher.txt');
+});
+
+app.get('/users/gofurjon', (req,res) => {
+    res.sendFile(__dirname + '/gofurjon.txt');
+});
+
+// app.post('/all', (req,res) => {
+//     res.send(`<h1Port ishlamoqda</h1>`);
+// });
 
 
 app.listen(port, () => {
